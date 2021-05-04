307458f85d1784fa4c4839017ee3a3e0ed66aa6e
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-05-04 19:37:47 +0900

    UI(Blockly)実装のためのコード追加

--- myblock.js
+++ myblock.js
@@ -1,6 +1,7 @@
 Blockly.defineBlocksWithJsonArray(
     /* ここにJSONファイルの内容をコピー */
-    [{
+    [
+        {
         "type": "go_ahead",
         "message0": "まっすぐ進む",
         "inputsInline": true,
@@ -9,47 +10,28 @@ Blockly.defineBlocksWithJsonArray(
         "colour": 230,
         "tooltip": "",
         "helpUrl": ""
-    },
+        },
         {
-            "type": "turn",
-            "message0": "%1",
-            "args0": [
-                {
-                    "type": "field_dropdown",
-                    "name": "turn",
-                    "options": [
-                        [
-                            "右へ向く",
-                            "turn_right"
-                        ],
-                        [
-                            "左を向く",
-                            "turn_left"
-                        ],
-                        [
-                            "",
-                            ""
-                        ]
-                    ]
-                }
-            ],
+            "type": "turn_right",
+            "message0": "右へ向く",
             "inputsInline": true,
             "previousStatement": null,
             "nextStatement": null,
-            "colour": 230,
+            "colour": 135,
             "tooltip": "",
             "helpUrl": ""
-        }]
+        }
+    ]
 );
-Blockly.JavaScript['go_ahead'] = function(block) {
-    // TODO: Assemble JavaScript into code variable.
-    var code = '...;\n';
-    return code;
-};
 
-Blockly.JavaScript['turn'] = function(block) {
-    var dropdown_turn = block.getFieldValue('turn');
-    // TODO: Assemble JavaScript into code variable.
-    var code = '...;\n';
+Blockly.JavaScript['go_ahead'] =  function(block) {
+    /* ここに実行したい処理を書く！！（まっすぐボタン） */
+    var code = 'console.log("まっすぐ進む");\n';
     return code;
-};
+}
+
+Blockly.JavaScript['turn_right'] =  function(block) {
+    /* ここに実行したい処理を書く！！（右向くボタン） */
+    var code = 'console.log("右へ向く");\n';
+    return code;
+}
