2485f2356e84a808d6ba74f570d54a47b5ecf2db
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-05-20 14:18:43 +0900

    map_view の置き換え

--- js/map_view.js
+++ js/map_view.js
@@ -1,22 +1,21 @@
 class MapView extends Object {
 
     // コンストラクタ
-    constructor(canvas, src) {
+    map;
+    constructor(src, width, height) {
         super();
         // playerの画像を設定
-        this.img = new Image();
-        this.img.src = src;
-        // canvasはhtmlとリンクしている
-        this.canvas = canvas;
+        let image = new PIXI.Texture.from(src);
+        this.map = new PIXI.Sprite(image);
     }
 
     // 画像を描画する関数(img, x座標, y座標, 画像の幅, 画像の高さ)
     draw(){
-        this.canvas.getContext('2d').drawImage(this.img, 0, 0, window.innerWidth / 2, window.innerHeight);
+        //this.canvas.getContext('2d').drawImage(this.img, 0, 0, window.innerWidth / 2, window.innerHeight);
     }
 
     drawMap(ctx, width, height){
-        console.log("マップビュー")
-        ctx.drawImage(this.img, this.x, this.y, width, height);
+        //console.log("マップビュー")
+        //ctx.drawImage(this.img, this.x, this.y, width, height);
     }
 }
