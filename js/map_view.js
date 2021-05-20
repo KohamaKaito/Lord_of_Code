b4aa8ae166ded0688030428fff8e1bd98dbbac2f
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2021-05-20 15:57:20 +0900

    マップの width と height を指定

--- js/map_view.js
+++ js/map_view.js
@@ -9,6 +9,8 @@ class MapView extends Object {
         this.map = new PIXI.Sprite(image);
         this.map.x = 0;
         this.map.y = 0;
+        this.map.width = width;
+        this.map.height = height;
     }
 
     // 画像を描画する関数(img, x座標, y座標, 画像の幅, 画像の高さ)
